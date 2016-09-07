bc42faeeb85ace0948313bc67fe6e2d1a7fe5df0
StarSkyLight <stark_09@foxmail.com>
2016-09-07 21:39:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ziyi/intentfilterpractice/AnotherActivity.java
@@ -0,0 +1,18 @@
+package com.example.ziyi.intentfilterpractice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ziyi on 2016/9/7.
+ */
+public class AnotherActivity extends AppCompatActivity{
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_another);
+        }
+
+}
